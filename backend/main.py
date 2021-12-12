4aba17d13d04c27ca733ff9d229190cf3cf69ea7
patrickstracey <pstracey17@gmail.com>
2021-12-12 09:28:09 -0800

    move app build to `main.py`

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,9 @@
-from api import app
+from flask import Flask
+from flask_restful import Api
+from resources.universalResource import universalEndpoint
+
+app = Flask(__name__)
+api = Api(app)
+
+
+api.add_resource(universalEndpoint, '/<string:collection>')
--- backend/resources/universalResource.py
+++ backend/resources/universalResource.py
@@ -2,11 +2,7 @@ from flask_restful import Resource
 from database.database import pullCollection
 
 
-class UniversalEndpoint(Resource):
+class universalEndpoint(Resource):
     def get(self, collection):
         result = pullCollection(collection)
         return result
-
-class GoodbyWorld(Resource):
-    def get(self):
-        return {'goodbye': 'world'}
